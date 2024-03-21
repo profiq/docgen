fdf7f068a2c39287960c2baee1e5edbbb8cc3942
Milos Svana <milos.svana@gmail.com>
2024-03-21 13:14:32 +0100

    Update tool versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.10.1"
+    id("org.jetbrains.intellij") version "1.17.1"
 }
 
 group = "com.profiq"
-version = "0.1.4"
+version = "0.1.5"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("junit:junit:4.13")
+    testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:3.12.4")
 }
 
@@ -19,9 +19,7 @@ dependencies {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     version.set("2023.3")
-    type.set("PC") // Target IDE Platform
-
-    plugins.set(listOf(/* Plugin Dependencies */))
+    plugins.set(listOf())
 }
 
 tasks {
