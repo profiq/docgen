940fae05b19396ee1936264c4efc225b3757f1a2
Milos Svana <milos.svana@gmail.com>
2024-05-29 13:47:13 +0200

    Tweak the looks of the setting screen

--- src/main/java/com/profiq/codexor/SettingsConfigurable.java
+++ src/main/java/com/profiq/codexor/SettingsConfigurable.java
@@ -40,6 +40,8 @@ public class SettingsConfigurable implements Configurable {
             PropertiesComponent.getInstance().setValue(MODEL_SETTINGS_KEY, currentModel);
         }
 
+        var maximumSize = new Dimension(600, 120);
+
         if (currentPrompt == null) {
             currentPrompt = PROMPT_DEFAULT;
             PropertiesComponent.getInstance().setValue(PROMPT_SETTINGS_KEY, currentPrompt);
@@ -58,96 +60,27 @@ public class SettingsConfigurable implements Configurable {
         promptField.setText(currentPrompt);
         promptField.setLineWrap(true);
         promptField.setWrapStyleWord(true);
-        promptField.setMaximumSize(new Dimension(600, 120));
+        promptField.setMaximumSize(maximumSize);
         promptField.setMinimumSize(new Dimension(270, 55));
         promptField.setRows(5);
 
         var instructions = new JBTextArea();
-        instructions.setText(
-            "* Leave the endpoint empty to use the default OpenAI API. If you want to use Azure Open AI, set put " +
-                "your endpoint URL here.");
+        instructions.setText("* Leave empty to use the default OpenAI API.");
         instructions.setLineWrap(true);
         instructions.setWrapStyleWord(true);
         instructions.setBackground(null);
         instructions.setEditable(false);
+        instructions.setMaximumSize(maximumSize);
         var font = instructions.getFont();
         instructions.setFont(new Font(font.getName(), Font.ITALIC, font.getSize() - 2));
 
-
         ui = FormBuilder.createFormBuilder()
-            .addLabeledComponent("Open AI API Endpoint*: ", endpointField)
-            .addLabeledComponent("OpenAI/Azure API Key: ", apiKeyField)
-            .addLabeledComponent("Model: ", modelField)
+            .addLabeledComponent("OpenAI/Azure API Key:", apiKeyField)
+            .addLabeledComponent("Model/azure deployment:", modelField)
+            .addLabeledComponent("Azure endpoint*:", endpointField)
             .addLabeledComponent("Prompt: ", promptField, 50, true)
             .addComponentFillVertically(instructions, 50)
             .getPanel();
-
-        /*
-
-        Dimension labelDimension = new Dimension(200, 30);
-
-        var endpointLabel = new JLabel();
-        endpointLabel.setText("Open AI API Endpoint: ");
-        endpointLabel.setPreferredSize(labelDimension);
-        endpointLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        endpointField = new JBTextField();
-        endpointField.setText("");
-        endpointField.setPreferredSize(new Dimension(600, 30));
-        endpointField.setToolTipText("You can set the Open AI API endpoint here. For example, you can set set it to your Azure Open AI deployment endpoint. Leave it empty to use the default OpenAI API endpoint.");
-
-        var apiKeyLabel = new JLabel();
-        apiKeyLabel.setText("OpenAI/Azure API Key: ");
-        apiKeyLabel.setPreferredSize(labelDimension);
-        apiKeyField = new JBTextField();
-        apiKeyField.setText(currentApiKey);
-        apiKeyField.setSize(250, 30);
-        apiKeyField.setPreferredSize(new Dimension(600, 30));
-        apiKeyField.setToolTipText("You can get your API key from https://openai.com/.");
-
-        var modelLabel = new JLabel();
-        modelLabel.setText("Model: ");
-        modelLabel.setPreferredSize(labelDimension);
-        modelField = new JBTextField();
-        modelField.setText(currentModel);
-        modelField.setSize(250, 30);
-        modelField.setPreferredSize(new Dimension(600, 30));
-
-        var promptLabel = new JLabel();
-        promptLabel.setText("Prompt: ");
-        promptLabel.setPreferredSize(labelDimension);
-        promptField = new JBTextArea();
-        promptField.setText(currentPrompt);
-        promptField.setSize(250, 30);
-        promptField.setPreferredSize(new Dimension(600, 120));
-        promptField.setLineWrap(true);
-        promptField.setWrapStyleWord(true);
-
-        ui = new JPanel();
-        var layout = new BoxLayout(ui, BoxLayout.Y_AXIS);
-        ui.setLayout(layout);
-        ui.setAlignmentX(Component.LEFT_ALIGNMENT);
-
-        ui.add(endpointLabel);
-        ui.add(endpointField);
-        ui.add(Box.createHorizontalGlue());
-
-        ui.add(Box.createRigidArea(new Dimension(0, 10)));
-        ui.add(apiKeyLabel);
-        ui.add(apiKeyField);
-
-        ui.add(Box.createRigidArea(new Dimension(0, 10)));
-        ui.add(modelLabel);
-        ui.add(modelField);
-
-        ui.add(Box.createRigidArea(new Dimension(0, 10)));
-        ui.add(promptLabel);
-        ui.add(promptField);
-
-        ui.add(Box.createGlue());
-        var filler = new JPanel();
-        filler.setPreferredSize(JBUI.size(0, 0));
-        ui.add(filler);
-        */
     }
 
     @Override
